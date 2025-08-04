40f1f0688c9c7150a85af97d1b7e1f6b976b66d2
shivesh501 <42916808+shivesh501@users.noreply.github.com>
2025-08-04 23:09:34 +0530

    Update posts.js

--- src/API/posts.js
+++ src/API/posts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://simple-blog-app-2oxx.onrender.com/posts"
+    baseURL: "https://simple-blog-app-2oxx.onrender.com"
 });
